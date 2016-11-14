efed873125b5ba7b9402e15797678dc3b298454a
flopez <lopez.fernando.damian@gmail.com>
2016-11-14 16:02:40 -0300

    Improved cpp key regex

--- tchecker/parsing_utils.py
+++ tchecker/parsing_utils.py
@@ -35,7 +35,7 @@ def get_resources_from_php_file(file_path):
 
 
 def get_resources_from_cpp_file(file_path):
-    return _get_resources_for_file_and_regex(r'(SSF_TRANS\w+)', file_path)
+    return _get_resources_for_file_and_regex(r'(SSF_TRANS_\w+)', file_path)
 
 
 def get_all_keys_from_lang(file_path):
